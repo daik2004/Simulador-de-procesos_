a390f5fbd24a685edbc9d328d034606a0bfc99b2
daiiiii <daisandif@gmail.com>
2025-08-10 00:34:27 -0600

    Se rediseño los bordes del form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -279,6 +279,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Simulador de procesos";
             this.tabControl1.ResumeLayout(false);
